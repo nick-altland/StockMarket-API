0a2e762dabe3d3d59634299d9b87ac1e6e59ac06
nick-altland <97930610+nick-altland@users.noreply.github.com>
2023-04-20 23:16:35 -0400

    Initial commit

---
+++ Investment.h
@@ -0,0 +1,67 @@
+/*
+ * Nicholas Altland
+ * CS120B: Open Ended Project
+ *
+ * Stock Market API: Investment Super Class header file
+ *
+ */
+
+#ifndef M2OEP_NALTLAND_INVESTMENT_H
+#define M2OEP_NALTLAND_INVESTMENT_H
+
+#include <string>
+
+// Need to add a method that calculates the returns on an investment (both bond and stock)
+
+class Investment {
+
+protected:
+    std::string companyName;
+    float initialInvestment;
+    float yearlyReturns = 0.0;
+
+public:
+    /**
+     * Requires: nothing
+     * Modifies:
+     * Effects: Creates an investment class
+     */
+    Investment();
+
+    /**
+     * Requires: nothing
+     * Modifies: company name and initialInvestment
+     * Effects: Sets the company name and the initial investment
+     */
+    explicit Investment(std::string companyName, float initialInvestment);
+
+
+    /**
+     * Requires: nothing
+     * Modifies: nothing
+     * Effects: Nothing, allows subclasses to overwrite it
+     */
+    virtual ~Investment() = default;
+
+    // Getters
+    virtual std::string getCompanyName() const;
+    virtual float getInitialInvestment() const;
+    virtual float getYearlyReturns() const;
+
+    // Setters
+    void setCompanyName(std::string companyName);
+    void setInitialInvestment(float initialInvestment);
+    void setYearlyReturns(float yearlyReturns);
+
+    // Overload Operands
+    friend  bool operator == (const Investment &first, const Investment &second);
+    friend bool operator > (const Investment &first, const Investment &second);
+    friend bool operator < (const Investment &first, const Investment &second);
+    friend bool operator != (const Investment &first, const Investment &second);
+    friend bool operator <= (const Investment &first, const Investment &second);
+    friend bool operator >= (const Investment &first, const Investment &second);
+
+};
+
+
+#endif //M2OEP_NALTLAND_INVESTMENT_H
